b9c2bb76eb6c17d3c63d47ca21f7f3f70ac28223
Olivier Oriol <olivier.oriol@aetna-softwares.com>
2018-07-14 12:08:48 +0200

    fix relm

--- server/src/ext/VeloxUserManagment.js
+++ server/src/ext/VeloxUserManagment.js
@@ -952,8 +952,8 @@ class VeloxUserManagment{
                                         let params = [] ;
                                         let where = "";
                                         if(realmColPath.length === 1){
-                                            where = table.name+"."+realmColPath[0]+ " = $"+params.length ;
                                             params.push(record[realmColPath[0]]) ;
+                                            where = table.name+"."+realmColPath[0]+ " = $"+params.length ;
                                         }else if(realmColPath.length>1){
                                             let whereCols = getJoinPairsFromFk(this.cache.schema, table.name, realmColPath[0]) ;
                                             where = Object.keys(whereCols).map((thisCol)=>{
